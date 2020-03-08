9f97bcc8fb49ea676e1a1420b6395b4fa02b0c8b
YaniPrieto <prietojohnchristian@gmail.com>
2020-03-08 14:56:02 +0800

    f

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function loadLabeledImages() {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
         // const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`https://github.com/YaniPrieto/FaceRecognitionApi/master/labeled_images${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
