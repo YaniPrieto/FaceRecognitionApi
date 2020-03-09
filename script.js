2eb19fe5948de3556c9069b00931ccabb18df81c
YaniPrieto <prietojohnchristian@gmail.com>
2020-03-09 08:01:47 +0800

    3rd commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function start() {
   const container = document.createElement('div')
   container.style.position = 'relative'
   document.body.append(container)
-  const labeledFaceDescriptors =   loadLabeledImages()
+  const labeledFaceDescriptors = await loadLabeledImages()
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
   let image
   let canvas
@@ -36,12 +36,12 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark','ZAlat']
+  const labels = ['BlackWidow', 'CaptainAmerica', 'CaptainMarvel', 'Hawkeye', 'JimRhodes', 'Thor', 'TonyStark','ZAlat','YaniPrieto']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://github.com/YaniPrieto/FaceRecognitionApi/tree/master/labeled_images${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/YaniPrieto/FaceRecognitionApi/master/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
